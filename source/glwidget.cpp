2bbdbc8d5e24028fcff5a7aa091ce337871ea54f
sauer <sauer@ient.rwth-aachen.de>
2015-11-12 17:20:44 +0100

    removed obsolete code

--- source/glwidget.cpp
+++ source/glwidget.cpp
@@ -48,7 +48,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include "glm/gtx/string_cast.hpp"
 // ImageMagick to read bmp
-#include <Magick++.h>
+
+#include <QElapsedTimer>
 
 #include <iostream>
 
@@ -57,10 +58,7 @@
 
 GLWidget::GLWidget(QWidget *parent)
     : QOpenGLWidget(parent),
-      m_vertice_indices_Vbo(QOpenGLBuffer::IndexBuffer),
-      m_xRot(0),
-      m_yRot(0),
-      m_zRot(0),
+      m_vertice_indices_Vbo(QOpenGLBuffer::IndexBuffer), 
       m_program(0),
       m_texture_data(0),
       m_frameHeight(1),
@@ -104,50 +102,9 @@ int GLWidget::heightForWidth(int w) const
     return h;
 }
 
-static void qNormalizeAngle(int &angle)
-{
-    while (angle < 0)
-        angle += 360 * 16;
-    while (angle > 360 * 16)
-        angle -= 360 * 16;
-}
-
-void GLWidget::setXRotation(int angle)
-{
-    qNormalizeAngle(angle);
-    if (angle != m_xRot) {
-        m_xRot = angle;
-        emit xRotationChanged(angle);
-        update();
-    }
-//    QOpenGLWidget::setFixedSize(200,200);
-//    setGeometry();
-}
-
-void GLWidget::setYRotation(int angle)
-{
-    qNormalizeAngle(angle);
-    if (angle != m_yRot) {
-        m_yRot = angle;
-        emit yRotationChanged(angle);
-        update();
-    }
-}
-
-void GLWidget::setZRotation(int angle)
-{
-    qNormalizeAngle(angle);
-    if (angle != m_zRot) {
-        m_zRot = angle;
-        emit zRotationChanged(angle);
-        update();
-    }
-}
-
 void GLWidget::cleanup()
 {
     makeCurrent();
-    m_logoVbo.destroy();
     m_vertices_Vbo.destroy();
     m_texture_coordinates_Vbo.destroy();
     m_depth_Vbo.destroy();
@@ -160,44 +117,19 @@ void GLWidget::cleanup()
 
 void GLWidget::cameraSelectionChanged(QItemSelection newCamera)
 {
-//    qDebug() << "tst" << newCamera.first();
-///    ... how to acces it/ cast it/ whatever ...
-///
     if(newCamera.isEmpty()) return;
-    qDebug() << "tst" << newCamera.first();
-///    ... how to acces it/ cast it/ whatever ...
-///
+
     CameraParameterSet camParams = newCamera.indexes().first().data(Qt::UserRole).value<CameraParameterSet>();
 
     m_K_view = camParams.getK();
     m_R_view = camParams.getR();
     m_t_view = camParams.getT();
 
-    // change matrices according to openGL style (negating third columns of K and R)
-//    m_K_view[2] = -1.f * m_K_view[2];
-//    m_R_view[2] = -1.f * m_R_view[2];
-//    std::cout << "det of R: " << glm::determinant(m_R_view) << std::endl;
-
-//    // change matrices according to openGL style (negating third columns of K and R)
-//    glm::mat3 changeSigns(1.f);
-//    // negate 2nd colum of K and 2nd row of R
-//    changeSigns[1][1] = -1.f;
-//    m_R_view = changeSigns * m_R_view ;
-//    m_K_view = m_K_view * changeSigns;
-//    // negate third colum of R and K
-//    changeSigns[1][1] = 1.f;
-//    changeSigns[2][2] = -1.f;
-//    m_R_view = m_R_view * changeSigns;
-//    m_K_view = m_K_view * changeSigns;
-
-
     //recompute opengl matrices
     setupMatrices();
 
     // draw from new perspective
-//    paintGL();
     update();
-
 }
 
 void GLWidget::updateFrame(const QImage &textureData, const QVector<uint8_t> &depthData)
@@ -240,6 +172,8 @@ void GLWidget::updateFrame(const QImage &textureData, const QVector<float> &dept
 
 void GLWidget::updateFrame(const QImage &textureData, const QByteArray &depthData)
 {
+    QElapsedTimer timer;
+    timer.start();
     // Set new texture
 //    QImage texture = QImage(reinterpret_cast<const unsigned char*>(textureData.constData()),m_frameWidth,m_frameHeight,m_textureFormat);
     m_texture_data = std::make_shared<QOpenGLTexture>(textureData,QOpenGLTexture::DontGenerateMipMaps);
@@ -250,23 +184,12 @@ void GLWidget::updateFrame(const QImage &textureData, const QByteArray &depthDat
     // Wrap texture coordinates by repeating
     m_texture_data->setWrapMode(QOpenGLTexture::Repeat);
 
-//    QVector<float> depthDataVec;
-//    for(int i=0;i<depthData.count();i++)
-//    {
-////        uint8_t *test = depthData.at(i);
-//        int test2 = depthData.at(i);
-////        if(test2<0)
-////        {
-//            uint8_t test3 = test2;
-////        }
-
-//        depthDataVec.append((float) test3);
-//    }
-
     // set new depth data
     m_depth_Vbo.bind();
     m_depth_Vbo.allocate(depthData.constData(), depthData.count() * sizeof(GLbyte));
 //    m_depth_Vbo.allocate(depthDataVec.constData(), depthDataVec.count() * sizeof(GLfloat));
+
+    qDebug() << "Moving data to graphics card took" << timer.elapsed() << "milliseconds";
 
     update();
 }
@@ -377,10 +300,6 @@ void GLWidget::initializeGL()
 
     m_matMVP_Loc = m_program->uniformLocation("MVP");
     m_matK2_inv_Loc = m_program->uniformLocation("K2_inv");
-    m_matProjectXY_Loc = m_program->uniformLocation("matProjectXY_from_ref_to_virt");
-    m_matProjectZ_Loc = m_program->uniformLocation("matProjectZ_from_ref_to_virt");
-    m_matProjectXY_world_Loc = m_program->uniformLocation("matProjectXY_from_ref_to_virt_world");
-    m_matProjectZ_world_Loc = m_program->uniformLocation("matProjectZ_from_ref_to_virt_world");
 
     m_vertices_Loc = m_program->attributeLocation("vertexPosition_modelspace");
     m_texture_Loc = m_program->attributeLocation("vertexUV");
@@ -399,12 +318,6 @@ void GLWidget::initializeGL()
     m_vertice_indices_Vbo.create();
     m_texture_coordinates_Vbo.create();
     m_depth_Vbo.create();
-
-//    updateFormat();
-
-//    loadVideoData();
-
-
 
     // Store the vertex attribute bindings for the program.
     setupVertexAttribs();
@@ -470,50 +383,6 @@ void GLWidget::setupVertexAttribs()
 
 void GLWidget::setupMatrices()
 {
-//    glm::mat3x4 P2t(0.8948549867753071,   -0.05046163984489068, 411.5970167968247, -9060.076341977459,
-//                 0.01206148241001237,   0.9999514240487639, -43.61722592969886, -186.4324231963577,
-//                -9.195396620947882e-05, 6.830175425923271e-06, 1.070481143014745, 0.5386813524799995);
-
-//    m_P2 = glm::transpose(P2t);
-//    std::cout << "P2: " << glm::to_string(m_P2) << std::endl;
-
-//    m_mat3ProjectXY_from_ref_to_virt[0] = glm::vec3(m_P2[0]);
-//    m_mat3ProjectXY_from_ref_to_virt[1] = glm::vec3(m_P2[1]);
-//    m_mat3ProjectXY_from_ref_to_virt[2] = glm::vec3(m_P2[2]);
-//    std::cout << "mat3ProjectXY_from_ref_to_virt: " << glm::to_string(m_mat3ProjectXY_from_ref_to_virt) << std::endl;
-
-//    m_vec3ProjectZ_from_ref_to_virt = m_P2[3];
-//    std::cout << "vec3ProjectZ_from_ref_to_virt: " << glm::to_string(m_vec3ProjectZ_from_ref_to_virt) << std::endl;
-
-//    glm::mat4x4 P3t( 0.000569053341143,  -0.000033009518064,  -0.360748429344014,  -5.541990934259999,
-//                    0.000033645245483,   0.000576968745365,  -0.335637862771796,  -0.264130025039997,
-//                    -0.000091953966209,   0.000006830175426,   1.070481143014745,   0.538681352479999,
-//                    0,  -0.000000000000000,   0.000000000000000,   1.000000000000000);
-//    m_P3 = glm::transpose(P3t);
-//    std::cout << "P3: " << glm::to_string(m_P3) << std::endl;
-
-//    m_mat3x4ProjectXY_from_ref_to_virt_world[0] = glm::vec4(m_P3[0]);
-//    m_mat3x4ProjectXY_from_ref_to_virt_world[1] = glm::vec4(m_P3[1]);
-//    m_mat3x4ProjectXY_from_ref_to_virt_world[2] = glm::vec4(m_P3[2]);
-//    std::cout << "mat3x4ProjectXY_from_ref_to_virt_world: " << glm::to_string(m_mat3x4ProjectXY_from_ref_to_virt_world) << std::endl;
-
-//    m_vec4ProjectZ_from_ref_to_virt_world = glm::vec4(m_P3[3]) ;
-//    std::cout << "vec4ProjectZ_from_ref_to_virt_world: " << glm::to_string(m_vec4ProjectZ_from_ref_to_virt_world) << std::endl;
-
-//    vec3 vertexPosition_modelspace;
-//    vec4 virt_position_world = vec4(matProjectXY_from_ref_to_virt_world * vec3(vertexPosition_modelspace[0],vertexPosition_modelspace[1],1));
-
-    // Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-
-
-// get opengl projection matrix from calibration matrix of camera, have to assume near and far clipping plane distances n,f
-//    K2: calibration matrix of second camera
-//    [1728.807356436687, 0, 967.099291776573;
-//      0, 1727.183523297295, 500.7943988015505;
-//      0, 0, 1]
-
-
-
 
  // parameters of view 0 hardcoded
     // calibration matrix
@@ -580,74 +449,13 @@ void GLWidget::setupMatrices()
     std::cout << "m_P_moveFromReferenceToVirtualView: " << glm::to_string(m_P_moveFromReferenceToVirtualView) << std::endl;
 
 
-
-
-
-
-
-
     float n = 15.0f;
     float f = 150.f;
     m_K_projectVirtualViewToImage = getProjectionFromCamCalibration(m_K_view,f,n);
 
-//    glm::mat4
-//            Projection = glm::perspective(45.0f, 16.0f / 9.0f, 0.1f, 500.0f);
-    // Camera matrix
-//position: -0.067845 0.459467 5.73404
-//position: -10.8117 -5.4518 926.9588
-//position+direction: -0.00739411 0.47914 4.73606
+    // Our ModelViewProjection : projection of model to different view and then to image
+    m_MVP        = m_K_projectVirtualViewToImage * m_P_moveFromReferenceToVirtualView;
 
-
-//    matWCoordFrame2CrefCoordFrame
-//    [0.466974, -0.484961, 0.739424, 17.95368067746;
-//      -0.763243, -0.643311, 0.060092, -28.04061200532;
-//      0.446537, -0.592422, -0.670552, 8.320918414620001;
-//      0, 0, 0, 1]
-//    glm::mat4 matWCoordFrame2CrefCoordFrame_t(
-//                0.466974, -0.484961, 0.739424, 17.95368067746,
-//                -0.763243, -0.643311, 0.060092, -28.04061200532,
-//                0.446537, -0.592422, -0.670552, 8.320918414620001,
-//                0,         0,          0,       1);
-//    m_matWCoordFrame2CrefCoordFrame = glm::transpose(matWCoordFrame2CrefCoordFrame_t);
-
-//    glm::mat4 View       = m_matWCoordFrame2CrefCoordFrame;
-//    // if I give the camera positive z position I have to multiply z by -1.0 in shader, to avoid clipping
-//    glm::mat4 View       = glm::lookAt(
-//                                glm::vec3(0,0,+0.10), // Camera position in World Space
-//                                glm::vec3(0,0,0), // looks at
-//                                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-//                           );
-
-//    glm::perspective()
-
-//     looks good from here:
-//    position: -0.250721-0.486483-0.499824
-//    position+direction: -0.322914-0.4880520.497565
-//    glm::mat4 View       = glm::lookAt(
-//                                glm::vec3(-0.250721,-0.486483,-0.499824), // Camera is at (4,3,-3), in World Space
-//                                glm::vec3(-0.322914,-0.488052,0.497565), // and looks at the origin
-//                                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-//                           );
-//    glm::mat4 View       = glm::lookAt(
-//                                glm::vec3(width/2,height/2,2000), // Camera is at (4,3,-3), in World Space
-//                                glm::vec3(width/2,height/2,0), // and looks at the origin
-//                                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-//                           );
-    // Model matrix : an identity matrix (model will be at the origin)
-    m_Model     = glm::mat4(1.0f);
-//    Model               = glm::scale(Model, glm::vec3(0.5f,0.2f,2.f));
-//    Model               = glm::rotate(Model, 30.f, glm::vec3(0.0f,1.f,0.f));
-//    Model               = glm::rotate(Model, 30.f, glm::vec3(0.0f,0.f,1.f));
-//    Model               = glm::translate(Model, glm::vec3(0.0f,1.f,-1.f));
-
-
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    m_MVP        = m_K_projectVirtualViewToImage * m_P_moveFromReferenceToVirtualView; // * View * m_Model; // Remember, matrix multiplication is the other way around
-//    m_MVP        = m_Projection;
-//    glm::mat4 MVP        = Projection; // Remember, matrix multiplication is the other way around
-    std::cout << "Model: " << glm::to_string(m_Model) << std::endl;
-//    std::cout << "View: " << glm::to_string(View) << std::endl;
-    std::cout << "Projection: " << glm::to_string(m_Projection) << std::endl;
     std::cout << "MVP: " << glm::to_string(m_MVP) << std::endl;
 }
 
@@ -662,38 +470,16 @@ void GLWidget::paintGL()
     if(m_texture_data == NULL) return;
 
     m_texture_data->bind();
-    // Set our "myTextureSampler" sampler to user Texture Unit 0
-//    glUniform1i(TextureID, 0);
-    // Use texture unit 0 which contains cube.png
     m_program->setUniformValue("textureSampler", 0);
-
-//    glm::mat3x3 mat_out;
-//    glGetUniformfv(programID, K2_inv_ID, &mat_out[0][0]);
-//    std::cout << "mat_out: " << glm::to_string(mat_out) << std::endl;
 
     QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
     m_program->bind();
-//    m_program->setUniformValue(m_projMatrixLoc, m_proj);
-//    m_program->setUniformValue(m_mvMatrixLoc, m_camera * m_world);
-//    QMatrix3x3 normalMatrix = m_world.normalMatrix();
-//    m_program->setUniformValue(m_normalMatrixLoc, normalMatrix);
 
-//    m_program->setUniformValue(m_matMVP_Loc,&m_MVP[0][0]);
-//    m_program->setUniformValue(m_matK2_inv_Loc,m_K2_inv);
-//    m_program->setUniformValue(m_matProjectXY_Loc,m_mat3ProjectXY_from_ref_to_virt);
-//    m_program->setUniformValue(m_matProjectZ_Loc,m_vec3ProjectZ_from_ref_to_virt);
-//    m_program->setUniformValue(m_matProjectXY_world_Loc,m_mat3x4ProjectXY_from_ref_to_virt_world);
-//    m_program->setUniformValue(m_matProjectZ_world_Loc,m_vec4ProjectZ_from_ref_to_virt_world);
     glUniformMatrix4fv(m_matMVP_Loc, 1, GL_FALSE, &m_MVP[0][0]);
     glUniformMatrix3fv(m_matK2_inv_Loc, 1, GL_FALSE, &m_Kinv_Cref[0][0]);
-//    glUniformMatrix3fv(m_matProjectXY_Loc, 1, GL_FALSE, &m_mat3ProjectXY_from_ref_to_virt[0][0]);
-//    glUniform3fv(m_matProjectZ_Loc, 1, &m_vec3ProjectZ_from_ref_to_virt[0]);
-//    glUniformMatrix3x4fv(m_matProjectXY_world_Loc, 1, GL_FALSE, &m_mat3x4ProjectXY_from_ref_to_virt_world[0][0]);
-//    glUniform4fv(m_matProjectZ_world_Loc, 1, &m_vec4ProjectZ_from_ref_to_virt_world[0]);
 
     m_vertice_indices_Vbo.bind();
 
-//    glDrawArrays(GL_TRIANGLES, 0, m_logo.vertexCount());
     glDrawElements(
         GL_TRIANGLES,      // mode
         m_videoFrameTriangles_indices.size(),    // count
@@ -707,30 +493,9 @@ void GLWidget::paintGL()
 
 void GLWidget::resizeGL(int w, int h)
 {
-    ///TODO:
-//    m_proj.setToIdentity();
-//    m_proj.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
+    ///TODO: i don't think i have to do anything here
 }
 
-void GLWidget::mousePressEvent(QMouseEvent *event)
-{
-    m_lastPos = event->pos();
-}
-
-void GLWidget::mouseMoveEvent(QMouseEvent *event)
-{
-    int dx = event->x() - m_lastPos.x();
-    int dy = event->y() - m_lastPos.y();
-
-    if (event->buttons() & Qt::LeftButton) {
-        setXRotation(m_xRot + 8 * dy);
-        setYRotation(m_yRot + 8 * dx);
-    } else if (event->buttons() & Qt::RightButton) {
-        setXRotation(m_xRot + 8 * dy);
-        setZRotation(m_zRot + 8 * dx);
-    }
-    m_lastPos = event->pos();
-}
 
 glm::mat4 GLWidget::getProjectionFromCamCalibration(glm::mat3 &calibrationMatrix, float clipFar, float clipNear)
 {
@@ -764,42 +529,6 @@ glm::mat4 GLWidget::getProjectionFromCamCalibration(glm::mat3 &calibrationMatrix
     return Projection2;
 }
 
-void GLWidget::loadVideoData(){
-
-
-    // Load the texture using any two methods
-//    loadTexture_from_BMP("Poznan_Blocks_t0_1920x1080_25_f1.bmp");
-
-
-    // Load cube.png image
-    QImage test = QImage("Poznan_Blocks_t0_1920x1080_25_f1.bmp").mirrored();
-
-
-    m_texture_data = std::make_shared<QOpenGLTexture>(QImage(test.constBits(),test.width(),test.height(),test.format()),QOpenGLTexture::DontGenerateMipMaps);
-//    m_texture_data = new QOpenGLTexture(QImage("Poznan_Blocks_t0_1920x1080_25_f1.bmp").mirrored(),QOpenGLTexture::DontGenerateMipMaps);
-
-    // Set nearest filtering mode for texture minification
-    m_texture_data->setMinificationFilter(QOpenGLTexture::Nearest);
-
-    // Set bilinear filtering mode for texture magnification
-    m_texture_data->setMagnificationFilter(QOpenGLTexture::Linear);
-
-    // Wrap texture coordinates by repeating
-    // f.ex. texture coordinate (1.1, 1.2) is same as (0.1, 0.2)
-    m_texture_data->setWrapMode(QOpenGLTexture::Repeat);
-
-    // depthmap data
-    loadDepthMap("Poznan_Blocks_d0_1920x1080_25_f1_padded.bmp", m_frameWidth+2, m_frameHeight+2);
-
-    m_depth_Vbo.bind();
-//    QImage test2  = QImage("Poznan_Blocks_d0_1920x1080_25_f1_padded.bmp");
-//    test.constBits()
-//    m_depth_Vbo.allocate(test.constBits(), test.size().width()*test.size().height() * sizeof(GLubyte));
-    m_depth_Vbo.allocate(m_depth_data.constData(), m_depth_data.count() * sizeof(GLfloat));
-
-
-
-}
 
 // function to generate the vertices corresponding to the pixels of a frame. Each vertex is centered at a pixel, borders are padded.
 void GLWidget::computeFrameVertices(int frameWidth, int frameHeight)
@@ -866,77 +595,3 @@ void GLWidget::computeTextureCoordinates(int frameWidth, int frameHeight)
     }
 }
 
-
-void GLWidget::loadDepthMap(const char * imagepath, int frameWidth, int frameHeight){
-
-//    float * data_float;
-    int imageSize;
-
-    printf("Reading image %s\n", imagepath);
-
-    Magick::InitializeMagick(NULL);
-
-    // Construct the image object. Separating image construction from the
-    // the read operation ensures that a failure to read the image file
-    // doesn't render the image object useless.
-    Magick::Image image;
-    try {
-        // Read a file into image object
-        image.read( imagepath );
-
-        int width = image.columns();
-        int height = image.rows();
-
-        std::cout << image.magick() << std::endl;
-
-        imageSize = width*height;
-        // convert to float
-//        data_float = new float [imageSize];
-        m_depth_data.resize(imageSize);
-
-        // get a "pixel cache" for the entire image
-        Magick::PixelPacket *pixels = image.getPixels(0, 0, width, height);
-
-        // now you can access single pixels like a vector
-        int row = 0;
-        int column = 0;
-        for(size_t h = 0; h < height; h++)
-        {
-            for(size_t w = 0; w < width; w++)
-            {
-                int i = h*width + w;
-
-                Magick::Color color = pixels[h * width + w];
-                int test = (unsigned char) color.redQuantum();
-
-//                if(data[i]<min) min = data[i];
-//                if(data[i]>max) max = data[i];
-    //            data_float[i] = w%50 + 50;
-                m_depth_data[i] = test;
-//                data_float[i] = test;
-            }
-        }
-
-
-
-//        pixels[0] = Magick::Color(255, 0, 0);
-        // Crop the image to specified size (width, height, xOffset, yOffset)
-//        image.crop( Geometry(100,100, 100, 100) );
-
-        // Write the image to a file
-//        image.write( "x.gif" );
-    }
-    catch( Magick::Exception &error_ )
-    {
-        std::cout << "Caught exception: " << error_.what() << std::endl;
-//        return 1;
-    }
-//    return 0;
-
-
-
-    std::cout << "Read " << imageSize << " depth values" << std::endl;
-//    std::cout << "With padding: " << imageSize << " depth values" << max << std::endl;
-//    std::cout << "min: " << min << " max: " << max << std::endl;
-
-}
--- source/glwidget.h
+++ source/glwidget.h
@@ -75,9 +75,6 @@ public:
     int heightForWidth(int w) const;
 
 public slots:
-    void setXRotation(int angle);
-    void setYRotation(int angle);
-    void setZRotation(int angle);
     void cleanup();
     void cameraSelectionChanged(QItemSelection newCamera);
     void updateFrame(const QImage &textureData, const QVector<float> &depthData); // display a new frame
@@ -87,87 +84,51 @@ public slots:
 
 
 signals:
-    void xRotationChanged(int angle);
-    void yRotationChanged(int angle);
-    void zRotationChanged(int angle);
+
 
 protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
     void resizeGL(int width, int height) Q_DECL_OVERRIDE;
-    void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
-    void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
 
 private:
+    void computeFrameVertices(int frameWidth, int frameHeight);
+    void computeFrameMesh(int frameWidth, int frameHeight);
+    void computeTextureCoordinates(int frameWidth, int frameHeight);
     void setupVertexAttribs();
     void setupMatrices();
     glm::mat4 getProjectionFromCamCalibration(glm::mat3 &calibrationMatrix, float clipFar, float clipNear);
-    void loadVideoData();
 
     bool m_core;
-    int m_xRot;
-    int m_yRot;
-    int m_zRot;
 
     int m_frameWidth;
     int m_frameHeight;
 
     QPoint m_lastPos;    
     QOpenGLVertexArrayObject m_vao;
-    QOpenGLBuffer m_logoVbo;
     QOpenGLBuffer m_vertices_Vbo;
     QOpenGLBuffer m_vertice_indices_Vbo;
     QOpenGLBuffer m_texture_coordinates_Vbo;
     QOpenGLBuffer m_depth_Vbo;
 
-    //    unsigned char * m_texture_data;
     std::shared_ptr<QOpenGLTexture> m_texture_data;
     QImage::Format m_textureFormat;
     QVector<GLfloat> m_vertices_data;
-//    QVector<GLfloat> m_texture_data;
     QVector<GLfloat> m_depth_data;
 
     QOpenGLShaderProgram *m_program;
-    int m_projMatrixLoc;
-    int m_mvMatrixLoc;
-    int m_normalMatrixLoc;
-    int m_lightPosLoc;        
-    // Get a handle for our "MVP" uniform
+
     int m_matMVP_Loc;
     int m_matK2_inv_Loc;
-    int m_matProjectXY_Loc;
-    int m_matProjectZ_Loc;
-    int m_matProjectXY_world_Loc;
-    int m_matProjectZ_world_Loc;
 
     // handles for texture, vertices and depth
     int m_vertices_Loc;
     int m_texture_Loc;
     int m_depth_Loc;
 
-
-
-    glm::mat4x3 m_P2;
-    glm::mat3 m_mat3ProjectXY_from_ref_to_virt;
-    glm::vec3 m_vec3ProjectZ_from_ref_to_virt;
-    glm::mat4x4 m_P3;
-    glm::mat3x4 m_mat3x4ProjectXY_from_ref_to_virt_world;
-    glm::vec4 m_vec4ProjectZ_from_ref_to_virt_world;
-
-    glm::mat3 m_Kinv_Cref;
-    glm::mat4 m_Projection;
-    glm::mat4 m_matWCoordFrame2CrefCoordFrame;
-    glm::mat4 m_View;
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 m_Model;
-    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat3 m_Kinv_Cref;    
     glm::mat4 m_MVP;
 
-    QMatrix4x4 m_proj;
-    QMatrix4x4 m_K_camera;
-    QMatrix4x4 m_world;
-
-    glm::mat3 m_K_sourceImage2World;
     glm::mat4 m_P_moveFromReferenceToVirtualView;
     glm::mat4 m_K_projectVirtualViewToImage;
 
@@ -182,11 +143,7 @@ private:
     std::vector<float> m_videoFrameTriangles_texture_uv;
 
     bool m_transparent;
-    void computeFrameVertices(int frameWidth, int frameHeight);
-    void computeFrameMesh(int frameWidth, int frameHeight);
-    bool loadTexture_from_BMP(const char *imagepath);
-    void loadDepthMap(const char *imagepath, int frameWidth, int frameHeight);
-    void computeTextureCoordinates(int frameWidth, int frameHeight);
+
 };
 
 #endif
